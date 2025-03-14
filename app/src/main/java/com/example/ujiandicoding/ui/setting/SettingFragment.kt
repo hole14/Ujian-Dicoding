b6e205c058377be25b8e31a3c668c9188c9d3d6a
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-03-14 09:01:44 +0700

    push

--- app/src/main/java/com/example/ujiandicoding/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/example/ujiandicoding/ui/setting/SettingFragment.kt
@@ -1,19 +1,41 @@
 package com.example.ujiandicoding.ui.setting
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import com.example.ujiandicoding.R
+import com.example.ujiandicoding.databinding.FragmentSettingBinding
 
 class SettingFragment : Fragment() {
-
+    private lateinit var binding: FragmentSettingBinding
+    private val viewModel: SettingViewModel by viewModels(){
+        ViewModelFactory(SettingPreference.getInstance(requireContext().dataStore))
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+    ): View {
+        binding = FragmentSettingBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.getThemeSetting().observe(viewLifecycleOwner){ isDarkModeActive ->
+            binding.switchTheme.isChecked = isDarkModeActive
+            binding.switchTheme.text = if (isDarkModeActive) "Light Mode" else "Dark Mode"
+        }
+
+        binding.switchTheme.setOnCheckedChangeListener { _, isChecked ->
+            viewModel.saveThemeSetting(isChecked)
+        }
+    }
+
 }

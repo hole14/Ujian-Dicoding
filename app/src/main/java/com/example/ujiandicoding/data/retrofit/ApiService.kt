899e8f534d8b493ade779813a5a9924412f5f29f
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-03-17 22:03:03 +0700

    push

---
+++ app/src/main/java/com/example/ujiandicoding/data/retrofit/ApiService.kt
@@ -0,0 +1,18 @@
+package com.example.ujiandicoding.data.retrofit
+
+import com.example.ujiandicoding.data.respone.DetailEventsResponse
+import com.example.ujiandicoding.data.respone.EventsResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("events")
+    suspend fun getUpcomingEvents(@Query("active") active: Int = 1): EventsResponse
+    suspend fun getFinishedEvents(@Query("active") active: Int = 0): EventsResponse
+    suspend fun getSearchEvents(@Query("active") active: Int = -1, @Query("q") q: String): EventsResponse
+
+    @GET("events/{id}")
+    suspend fun getDetailEvents(@Path("id") id: Int): DetailEventsResponse
+
+}

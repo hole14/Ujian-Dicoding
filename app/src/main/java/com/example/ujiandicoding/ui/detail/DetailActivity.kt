f5873dc1ccb3914fd872436ec99fc7eb6b6f400a
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-03-18 21:54:24 +0700

    push

--- app/src/main/java/com/example/ujiandicoding/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/example/ujiandicoding/ui/detail/DetailActivity.kt
@@ -1,19 +1,83 @@
 package com.example.ujiandicoding.ui.detail
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.text.HtmlCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.ujiandicoding.R
+import com.example.ujiandicoding.data.entity.EventEntity
+import com.example.ujiandicoding.databinding.ActivityDetailBinding
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class DetailActivity : AppCompatActivity() {
     companion object{
         const val EXTRA_EVENT = "extra_event"
     }
+    private lateinit var binding: ActivityDetailBinding
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        val event = intent.getParcelableExtra<EventEntity>(EXTRA_EVENT)
+
+        binding.tvJudul.text = event?.name
+        binding.tvKategori.text = event?.category
+        binding.tvTanggal.text = "📆 ${event?.beginTime?.let { formatDate(it) }} - ${event?.endTime?.let { formatDate(it) }}"
+        binding.tvWaktu.text = "⏱ ${event?.beginTime?.let { formatTime(it) }} - ${event?.endTime?.let { formatTime(it) }}"
+        binding.tvDeskripsi.text = HtmlCompat.fromHtml(event?.description!!, HtmlCompat.FROM_HTML_MODE_LEGACY)
+        Glide.with(this)
+            .load(event.mediaCover)
+            .apply(RequestOptions.placeholderOf(R.drawable.ic_loading).error(R.drawable.ic_error))
+            .into(binding.ivFoto)
+        binding.tvKota.text = "📍 ${event.cityName}"
+        binding.tvQuota.text = "👥 ${event.registrants}/${event.quota}"
+        binding.tvSummary.text = event.summary
+        binding.tvOwner.text = "By: ${event.ownerName}"
+        binding.btnRegister.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(event.link))
+            startActivity(intent)
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            onBackPressed()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun formatDate(date: String): String {
+        val inputFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+        val outputFormat = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault())
+
+        return try {
+            val parsedDate = inputFormat.parse(date)
+            outputFormat.format(parsedDate!!)
+        } catch (e: Exception) {
+            date
+        }
+    }
+    private fun formatTime(dateTimeString: String): String {
+        val inputFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+        val outputFormat = SimpleDateFormat("h:mm a", Locale.getDefault())
+        return try {
+            val date = inputFormat.parse(dateTimeString)
+            outputFormat.format(date!!)
+        } catch (e: Exception) {
+            dateTimeString
+        }
     }
 }

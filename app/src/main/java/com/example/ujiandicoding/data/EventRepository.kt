4b8c39db41421dc10e73630b0ac9978e39856488
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-03-18 08:51:26 +0700

    push

--- app/src/main/java/com/example/ujiandicoding/data/EventRepository.kt
+++ app/src/main/java/com/example/ujiandicoding/data/EventRepository.kt
@@ -19,7 +19,7 @@ class EventRepository private constructor(
     fun getUpcomingEvents(): LiveData<Result<List<EventEntity>>> = liveData(Dispatchers.IO) {
         emit(Result.Loading)
         try {
-            val respone = apiService.getUpcomingEvents()
+            val respone = apiService.getUpcomingEvents(1)
             val listEvent = respone.listEvents
             val eventList = listEvent?.map { event ->
                 val isFavorite = eventDao.isEventFavorited(event?.id!!)
@@ -50,6 +50,40 @@ class EventRepository private constructor(
         val localData: LiveData<Result<List<EventEntity>>> = eventDao.getAllEvent().map { Result.Success(it) }
         emitSource(localData)
 
+    }
+    fun getFinishedEvents(): LiveData<Result<List<EventEntity>>> = liveData(Dispatchers.IO) {
+        emit(Result.Loading)
+        try {
+            val respone = apiService.getFinishedEvents(0)
+            val listEvent = respone.listEvents
+            val eventList = listEvent?.map { event ->
+                val isFavorite = eventDao.isEventFavorited(event?.id!!)
+                EventEntity(
+                    event.id,
+                    event.name!!,
+                    event.summary!!,
+                    event.ownerName!!,
+                    event.cityName!!,
+                    event.beginTime!!,
+                    event.endTime!!,
+                    event.imageLogo!!,
+                    event.mediaCover!!,
+                    event.link!!,
+                    event.description!!,
+                    event.category!!,
+                    event.quota!!,
+                    event.registrants!!,
+                    isFavorite
+                )
+            }
+            eventDao.deleteAll()
+            eventList?.let { eventDao.insertEvent(it) }
+        } catch (e: Exception) {
+            Log.d("EventRepository", "getFinishedEvents: ${e.message.toString()} ")
+            emit(Result.Error(e.message.toString()))
+        }
+        val localData: LiveData<Result<List<EventEntity>>> = eventDao.getAllEvent().map { Result.Success(it) }
+        emitSource(localData)
     }
     companion object {
         @Volatile

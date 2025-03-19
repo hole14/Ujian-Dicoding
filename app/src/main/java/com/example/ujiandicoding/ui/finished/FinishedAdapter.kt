55add3f238404203904159661cf00ec02ad5ae2f
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-03-19 21:42:03 +0700

    push

--- app/src/main/java/com/example/ujiandicoding/ui/finished/FinishedAdapter.kt
+++ app/src/main/java/com/example/ujiandicoding/ui/finished/FinishedAdapter.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -16,8 +17,8 @@ import com.example.ujiandicoding.ui.detail.DetailActivity
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class FinishedAdapter: ListAdapter<EventEntity, FinishedAdapter.ViewHolder>(DIFF_CALLBACK)  {
-    class ViewHolder(private val binding: ItemFinishedBinding): RecyclerView.ViewHolder(binding.root) {
+class FinishedAdapter(private val onFavoriteClick: (EventEntity) -> Unit): ListAdapter<EventEntity, FinishedAdapter.ViewHolder>(DIFF_CALLBACK)  {
+    class ViewHolder(val binding: ItemFinishedBinding): RecyclerView.ViewHolder(binding.root) {
         @SuppressLint("SetTextI18n")
         fun bind(event: EventEntity) {
             binding.tvJudul.text = event.name
@@ -50,6 +51,15 @@ class FinishedAdapter: ListAdapter<EventEntity, FinishedAdapter.ViewHolder>(DIFF
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val event = getItem(position)
         holder.bind(event)
+        val ivFavorite = holder.binding.ivFavorite
+        if (event.isFavorite) {
+            ivFavorite.setImageDrawable(ContextCompat.getDrawable(ivFavorite.context, R.drawable.ic_love_change))
+        } else {
+            ivFavorite.setImageDrawable(ContextCompat.getDrawable(ivFavorite.context, R.drawable.ic_love))
+        }
+        ivFavorite.setOnClickListener {
+            onFavoriteClick(event)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {

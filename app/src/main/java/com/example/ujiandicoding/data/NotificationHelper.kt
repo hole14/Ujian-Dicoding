0355545c4a8cdd232d6c5b14248377c5036b2367
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-03-20 21:28:07 +0700

    push

---
+++ app/src/main/java/com/example/ujiandicoding/data/NotificationHelper.kt
@@ -0,0 +1,31 @@
+package com.example.ujiandicoding.data
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import com.example.ujiandicoding.R
+
+object NotificationHelper {
+    private const val CHANNEL_ID = "daily_reminder_channel"
+    private const val NOTIFICATION_ID = 101
+
+    fun showNotification(context: Context, title: String?, message: String?) {
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID, "Daily Reminder", NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+        val notification = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.baseline_notifications_24)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        notificationManager.notify(NOTIFICATION_ID, notification)
+    }
+
+}

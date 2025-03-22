bcbfc6d83688c991b0a116d44d51eee18d7f6a46
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-03-22 09:14:51 +0700

    push

--- app/src/main/java/com/example/ujiandicoding/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/example/ujiandicoding/ui/detail/DetailActivity.kt
@@ -5,15 +5,20 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.text.HtmlCompat
+import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
-import com.example.ujiandicoding.EventViewModel
 import com.example.ujiandicoding.R
+import com.example.ujiandicoding.data.EventRepository
 import com.example.ujiandicoding.data.entity.EventEntity
+import com.example.ujiandicoding.data.retrofit.ApiConfig
+import com.example.ujiandicoding.data.room.EventDatabase
 import com.example.ujiandicoding.databinding.ActivityDetailBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -22,8 +27,7 @@ class DetailActivity : AppCompatActivity() {
         const val EXTRA_EVENT = "extra_event"
     }
     private lateinit var binding: ActivityDetailBinding
-    private val viewModel: EventViewModel by viewModels ()
-    private var event: EventEntity? = null
+    private lateinit var eventRepository: EventRepository
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +38,7 @@ class DetailActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
 
-        event = intent.getParcelableExtra(EXTRA_EVENT)
+        val event = intent.getParcelableExtra<EventEntity>(EXTRA_EVENT)
 
         binding.tvJudul.text = event?.name
         binding.tvKategori.text = event?.category
@@ -42,31 +46,36 @@ class DetailActivity : AppCompatActivity() {
         binding.tvWaktu.text = "⏰ ${event?.beginTime?.let { formatTime(it) }} - ${event?.endTime?.let { formatTime(it) }}"
         binding.tvDeskripsi.text = HtmlCompat.fromHtml(event?.description!!, HtmlCompat.FROM_HTML_MODE_LEGACY)
         Glide.with(this)
-            .load(event?.mediaCover)
+            .load(event.mediaCover)
             .apply(RequestOptions.placeholderOf(R.drawable.ic_loading).error(R.drawable.ic_error))
             .into(binding.ivFoto)
-        binding.tvKota.text = "📍 ${event?.cityName}"
-        binding.tvQuota.text = "👥 ${event?.registrants}/${event?.quota}"
-        binding.tvSummary.text = event?.summary
-        binding.tvOwner.text = "By: ${event?.ownerName}"
+        binding.tvKota.text = "📍 ${event.cityName}"
+        binding.tvQuota.text = "👥 ${event.registrants}/${event.quota}"
+        binding.tvSummary.text = event.summary
+        binding.tvOwner.text = "By: ${event.ownerName}"
         binding.btnRegister.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(event?.link))
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(event.link))
             startActivity(intent)
         }
-//        binding.fabAdd.setOnClickListener {
-//            event?.let {
-//                val favorite = !it.isFavorite
-//                it.isFavorite = favorite
-//                viewModel.updateEventFavorite(it.id, favorite)
-//                updateFavoriteButton(favorite)
-//            }
-//        }
 
+        val database = EventDatabase.getInstance(this)
+        val eventDao = database.eventDao()
+        eventRepository = EventRepository.getInstance(eventDao, ApiConfig.getApiService())
+
+        binding.fabAdd.setOnClickListener {
+            lifecycleScope.launch(Dispatchers.IO) {
+                eventRepository.toggleFavorite(event)
+                val favorite = !event.isFavorite
+                event.isFavorite = favorite
+                withContext(Dispatchers.Main) {
+                    binding.fabAdd.setImageResource(
+                        if (favorite) R.drawable.ic_love_change else R.drawable.ic_love
+                    )
+                    binding.fabAdd.setColorFilter(binding.root.context.getColor(if (favorite) R.color.red else R.color.placeholder))
+                }
+            }
+        }
     }
-//    private fun updateFavoriteButton(isFavorite: Boolean) {
-//        val icon = if (isFavorite) R.drawable.ic_love else R.drawable.ic_love_change
-//        binding.fabAdd.setImageResource(icon)
-//    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == android.R.id.home) {
@@ -76,7 +85,7 @@ class DetailActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-    fun formatDate(date: String): String {
+    private fun formatDate(date: String): String {
         val inputFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
         val outputFormat = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault())
 

e9961e3fd39b22d50c81735cb5a3787285497fc1
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-03-22 09:15:06 +0700

    push

--- app/src/main/java/com/example/ujiandicoding/data/room/EventDatabase.kt
+++ app/src/main/java/com/example/ujiandicoding/data/room/EventDatabase.kt
@@ -4,21 +4,31 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.ujiandicoding.data.entity.EventEntity
 
-@Database(entities = [EventEntity::class], version = 1, exportSchema = false)
+@Database(entities = [EventEntity::class], version = 2, exportSchema = false)
 abstract class EventDatabase: RoomDatabase() {
     abstract fun eventDao():EventDao
     companion object{
         @Volatile
         private var instance: EventDatabase? = null
+        private val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+                db.execSQL("ALTER TABLE event ADD COLUMN active INTEGER NOT NULL DEFAULT 1")
+            }
+
+        }
         fun getInstance(context: Context): EventDatabase =
             instance ?: synchronized(this) {
                 instance ?: Room.databaseBuilder(
                     context.applicationContext,
                     EventDatabase::class.java,
                     "Event.db"
-                ).build()
+                )
+                    .addMigrations(MIGRATION_1_2)
+                    .build()
             }
     }
 }

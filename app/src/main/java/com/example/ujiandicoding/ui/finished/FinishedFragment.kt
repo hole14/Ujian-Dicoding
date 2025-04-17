665ea300b7b5d9f6ecb51597ea5b148d32d66909
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-04-17 11:15:26 +0700

    push

--- app/src/main/java/com/example/ujiandicoding/ui/finished/FinishedFragment.kt
+++ app/src/main/java/com/example/ujiandicoding/ui/finished/FinishedFragment.kt
@@ -1,15 +1,21 @@
 package com.example.ujiandicoding.ui.finished
 
+import android.graphics.PorterDuff
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.widget.SearchView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ujiandicoding.EventViewModel
 import com.example.ujiandicoding.EventViewModelFactory
+import com.example.ujiandicoding.R
 import com.example.ujiandicoding.data.Result
 import com.example.ujiandicoding.databinding.FragmentFinishedBinding
 
@@ -50,7 +56,22 @@ class FinishedFragment : Fragment() {
                 }
             }
         }
-        binding.searchView.setOnQueryTextListener(object : androidx.appcompat.widget.SearchView.OnQueryTextListener{
+
+        val searchText = binding.searchView.findViewById<EditText>(androidx.appcompat.R.id.search_src_text)
+        searchText.setHintTextColor(resources.getColor(android.R.color.darker_gray))
+        searchText.setTextColor(resources.getColor(android.R.color.black))
+
+        val searchIcon = binding.searchView.findViewById<ImageView>(androidx.appcompat.R.id.search_mag_icon)
+        searchIcon.setColorFilter(ContextCompat.getColor(requireContext(), R.color.search_icon_color), PorterDuff.Mode.SRC_IN)
+
+        val clearIcon = binding.searchView.findViewById<ImageView>(androidx.appcompat.R.id.search_close_btn)
+        clearIcon.setColorFilter(ContextCompat.getColor(requireContext(), R.color.search_icon_color), PorterDuff.Mode.SRC_IN)
+
+        binding.searchView.setIconifiedByDefault(false)
+        binding.searchView.queryHint = "Cari Event..."
+
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
             override fun onQueryTextSubmit(query: String?): Boolean {
                 return false
             }
